7e4f72c79e83f2ed1e248036958a3c34094a942d
Yassine <aameryassine@gmail.com>
2026-09-16 10:25:35 +0100

    fix config Supabase

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "const SUPABASE_URL = "https://wfrfjdwtjkpeyrmfyxgk.supabase.co";
+const SUPABASE_URL = "https://wfrfjdwtjkpeyrmfyxgk.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ZezTNP5jI0Jfztvjt43Y4g_uIuQPLrL";
